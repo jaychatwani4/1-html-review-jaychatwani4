3ce060947da861482f62199ff795293a5e76bbe2
Jay Chatwani <jaypchatwani@gmail.com>
2021-10-19 20:18:34 -0400

    Updates to JS

--- app/public/js/book.js
+++ app/public/js/book.js
@@ -3,7 +3,8 @@ const anotherApp = {
     data() {
       return {
         books: [],
-        bookForm: {}
+        bookForm: {},
+        selectedBook: null
       }
     },
     computed: {
@@ -17,6 +18,14 @@ const anotherApp = {
             const d = new Intl.NumberFormat("en-US").format(n);
             return "$ " + d;
         },
+        selectBook(s) {
+          if (s == this.selectedBook) {
+              return;
+          }
+          this.selectedBook = s;
+          this.books = [];
+          this.fetchBookData(this.selectedBook);
+        },
         fetchBookData() {
             fetch('/api/books/')
             .then( response => response.json() )
@@ -28,8 +37,39 @@ const anotherApp = {
                 console.error(err);
             })
         },
+        postBook(evt) {
+          console.log ("Test:", this.selectedBook);
+        if (this.selectedBook) {
+            this.postEditBook(evt);
+        } else {
+            this.postNewBook(evt);
+          }
+        },
+        postEditOffer(evt) {
+          // this.offerForm.id = this.selectedOffer.id;
+          // this.offerForm.studentId = this.selectedStudent.id;        
+          
+          // console.log("Editing!", this.offerForm);
+  
+          fetch('api/offer/update.php', {
+              method:'POST',
+              body: JSON.stringify(this.offerForm),
+              headers: {
+                "Content-Type": "application/json; charset=utf-8"
+              }
+            })
+            .then( response => response.json() )
+            .then( json => {
+              console.log("Returned from post:", json);
+              // TODO: test a result was returned!
+              this.offers = json;
+              
+              // reset the form
+              this.handleResetEdit();
+            });
+        },
         postNewBook(evt) {
-            this.bookForm.studentId = this.selectedStudent.id;        
+            this.bookForm.sr_no = this.selectedBook.sr_no;        
             console.log("Posting:", this.bookForm);
             // alert("Posting!");
     
@@ -44,7 +84,7 @@ const anotherApp = {
               .then( json => {
                 console.log("Returned from post:", json);
                 // TODO: test a result was returned!
-                this.offers = json;
+                this.books = json;
                 
                 // reset the form
                 this.bookForm = {};
@@ -52,7 +92,6 @@ const anotherApp = {
           }
     },
     created() {
-        // this.fetchUserData();
         this.fetchBookData();
     }
 
